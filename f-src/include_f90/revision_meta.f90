c42fe7f9063adfce34f2719a7757e61e73100b91
JoGebert <johannes.gebert@hlrs.de>
2022-01-01 23:22:45 +0100

    Squashed 'central_src/' changes from af5f3f9..4e0d4ff
    4e0d4ff Optical Enhancement
    1896378 Major rework/bugfixing done. Tests pending.
    028285b Reefactoring
    913e79a Merge commit 'e24981138adceb02c46aabd7fdaf6471962ed2f7' into integrate_meta_format
    c712ecc Merge commit '8d200ae8193f36ace991ca9f9cb4ea78d08ca8bd' into integrate_meta_format
    86a1d17 General refactoring
    6a65b7a Merge commit '03e34f7117752accee6afd702e4f5c78772dec3d' into integrate_meta_format
    329d9b8 Merge commit '69e4c5783078c89ff38bf13b6b96007dd2cabf29' into integrate_meta_format
    4eb100e Merge commit '2fd235347c9110fa52fbff75bcb2cfc06b559699' into create_subtree_handling
    ee6d322 Merge commit 'b4c8165935fb85db2e3bfce055f5e05e85408b17' into create_subtree_handling
    570a836 Merge commit '9aa0ac9d3a8631aa72eed2889c22045e76f8aa96' into create_subtree_handling
    72e633e Merge commit '972d3f5fd18ddd1098f8e5c78eb57da078905c95' into create_subtree_handling
    7bb34f8 Sourcing env now working
    
    git-subtree-dir: central_src
    git-subtree-split: 4e0d4ff80041b26cb00e65e24f45b3f72f178252

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,3 +1,3 @@
-CHARACTER(LEN=scl), PARAMETER :: longname = 'X to Meta Converter'
-CHARACTER(LEN=scl), PARAMETER :: revision = 'v0.1.0'
-CHARACTER(LEN=scl), PARAMETER :: hash = '8064a41484170c8289cd93f5aee1c9170f3ad62b'
+CHARACTER(LEN=scl), PARAMETER :: longname = 'Computed Tomography Image Filter'
+CHARACTER(LEN=scl), PARAMETER :: revision = 'V3.2.1'
+CHARACTER(LEN=scl), PARAMETER :: hash = '446f0274f28aa0b09118864602ed95c58c36000d'
