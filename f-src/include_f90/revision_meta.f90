fd9e75394aa1ed03db4ad6854f1d43916793ff4f
JoGebert <johannes.gebert@hlrs.de>
2022-01-15 00:53:34 +0100

    Squashed 'central_src/' changes from ecdbe64..98aaeca
    98aaeca Compiles, no unsused variables
    d6562d0 Compiles now
    07d73df Sire_stencil_v2
    af7121f content-related enhancements
    3cf47d2 Enhancing legacy code
    772c73d Test changes
    bddaf84 Additional dev
    592ea41 Refactored routines and variables
    40214b5 Subtree refactoring
    d8dd1df Updated central sources
    
    git-subtree-dir: central_src
    git-subtree-split: 98aaeca0662b5704ed363c3e4be132129a8c8fc2

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,3 +1,3 @@
-CHARACTER(LEN=scl), PARAMETER :: longname = 'CT-Binarization'
-CHARACTER(LEN=scl), PARAMETER :: revision = 'V1.0.0'
-CHARACTER(LEN=scl), PARAMETER :: hash = 'dc295231fc31255fdcf56e7ed72a667a18080055'
+CHARACTER(LEN=scl), PARAMETER :: longname = 'Spatial Image Registration'
+CHARACTER(LEN=scl), PARAMETER :: revision = ''
+CHARACTER(LEN=scl), PARAMETER :: hash = '2558ee36b5f5a83ba2423c89566e2a457d366e23'
