d4ed957262dc4b9943b1c63d98b613b2dfbccf10
JoGebert <johannes.gebert@hlrs.de>
2022-01-31 18:35:14 +0100

    Squashed 'central_src/' changes from 8b3aa07..1185ed6
    1185ed6 Bugfix @ effective stiffness
    bf01e02 Fixed bugs concerning petsc parts without boundary nodes
    91bff6b std enhancements
    9ff1015 'Bugfix'
    
    git-subtree-dir: central_src
    git-subtree-split: 1185ed605eaff4517230b8c82df82b8393fb9319

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,2 +1,2 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'Directly Discretizing Tensor Computation'
-CHARACTER(LEN=scl), PARAMETER :: hash = '0d655004c03a126db13ceab48270e802c639a01d'
+CHARACTER(LEN=scl), PARAMETER :: hash = '5d8ee2542c96ceaf8588c705acc6c4d3ba583015'
